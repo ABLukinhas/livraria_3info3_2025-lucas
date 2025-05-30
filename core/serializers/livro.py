1afff32bf26bfae7e41ca88253c7e18ae446801b
ABLukinhas <lucasantoniobrandao008@gmail.com>
2025-05-30 16:48:25 -0300

    feat: final

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -4,41 +4,13 @@ from core.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
-class LivroListSerializer(ModelSerializer):
 
+
+class LivroListSerializer(ModelSerializer):
     class Meta:
         model = Livro
-       
         fields = ("id", "titulo", "preco")
-
-
-
-class LivroRetrieveSerializer(ModelSerializer):
         
-    class Meta:
-        model = Livro
-        fields = "__all__"
-        depth = 1
-
-class LivroSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(
-        source='capa',
-        queryset=Image.objects.all(),
-        slug_field='attachment_key',
-        required=False,
-        write_only=True,
-    )
-    capa = ImageSerializer(required=False, read_only=True)
-
-    class Meta:
-        model = Livro
-        fields = "__all__"
-
-class LivroListSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = "__all__"
-
 
 class LivroRetrieveSerializer(ModelSerializer):
     capa = ImageSerializer(required=False)
@@ -47,7 +19,6 @@ class LivroRetrieveSerializer(ModelSerializer):
         model = Livro
         fields = '__all__'
         depth = 1
-
 
 class LivroSerializer(ModelSerializer):
     capa_attachment_key = SlugRelatedField(
