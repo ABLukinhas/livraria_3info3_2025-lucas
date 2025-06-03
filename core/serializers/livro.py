da535407a65763d6377b7beaf597f94cef0c21e3
ABLukinhas <lucasantoniobrandao008@gmail.com>
2025-06-03 08:49:38 -0300

    feat: inclusão da foto de perfil no usuário

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,16 +1,14 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
-
 from core.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 
-
 class LivroListSerializer(ModelSerializer):
     class Meta:
         model = Livro
-        fields = ("id", "titulo", "preco")
-        
+        fields = ('id', 'titulo', 'preco')
+
 
 class LivroRetrieveSerializer(ModelSerializer):
     capa = ImageSerializer(required=False)
@@ -19,6 +17,7 @@ class LivroRetrieveSerializer(ModelSerializer):
         model = Livro
         fields = '__all__'
         depth = 1
+
 
 class LivroSerializer(ModelSerializer):
     capa_attachment_key = SlugRelatedField(
